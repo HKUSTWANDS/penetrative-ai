43f0fac74accb47dddfa385f9a44cae8d2ed2b8f
xuhuatao <xht8448915123>
2023-09-29 21:52:56 +0800

    update content

--- static/js/index.js
+++ static/js/index.js
@@ -32,21 +32,21 @@ $(document).ready(function() {
     const copyButtonLabel = "Copy Code";
 
 // use a class selector if available
-    let blocks = document.querySelectorAll("pre");
-
-    blocks.forEach((block) => {
-        // only add button if browser supports Clipboard API
-        if (navigator.clipboard) {
-            let button = document.createElement("button");
-
-            button.innerText = copyButtonLabel;
-            block.appendChild(button);
-
-            button.addEventListener("click", async () => {
-                await copyCode(block, button);
-            });
-        }
-    });
+//     let blocks = document.querySelectorAll("pre");
+//
+//     blocks.forEach((block) => {
+//         // only add button if browser supports Clipboard API
+//         if (navigator.clipboard) {
+//             let button = document.createElement("button");
+//
+//             button.innerText = copyButtonLabel;
+//             block.appendChild(button);
+//
+//             button.addEventListener("click", async () => {
+//                 await copyCode(block, button);
+//             });
+//         }
+//     });
 
     var options = {
 			slidesToScroll: 1,
